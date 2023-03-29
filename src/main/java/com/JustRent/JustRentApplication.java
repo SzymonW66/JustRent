90c3e4652e563b48bfd7944acfd57530bc1b4da0
SzymonW66 <szymekwajs@gmail.com>
2023-03-29 22:14:31 +0200

    Initial commit

---
+++ src/main/java/com/JustRent/JustRentApplication.java
@@ -0,0 +1,13 @@
+package com.JustRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustRentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JustRent/JustRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JustRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
