53904f73c030548b3e99f885bfba4e5df54a5b22
SzymonW66 <szymekwajs@gmail.com>
2023-04-01 22:38:03 +0200

    Adding User class p2

---
+++ src/main/java/com/JustRent/model/User.java
@@ -0,0 +1,21 @@
+package com.JustRent.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "User")
+public class User {
+
+    @jakarta.persistence.Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    @Column (name = "name")
+    private String name;
+
+    @Column (name = "surname")
+    private String surname;
+}
