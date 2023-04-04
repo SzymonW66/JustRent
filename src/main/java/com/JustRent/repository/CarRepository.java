a6afcc36d256d33b2f930e5cdf333c18d01c0399
SzymonW66 <szymekwajs@gmail.com>
2023-04-04 21:18:07 +0200

    Adding CarRepository and UserRepository

---
+++ src/main/java/com/JustRent/repository/CarRepository.java
@@ -0,0 +1,10 @@
+package com.JustRent.repository;
+
+import com.JustRent.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
