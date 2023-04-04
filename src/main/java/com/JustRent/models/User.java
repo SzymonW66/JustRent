93f5ff15e6f70225ab74ca32f75d35f851accb34
SzymonW66 <szymekwajs@gmail.com>
2023-04-04 21:58:54 +0200

    Adding Getters, Setters and Constructor

--- src/main/java/com/JustRent/models/User.java
+++ src/main/java/com/JustRent/models/User.java
@@ -24,4 +24,34 @@ public class User {
 
     @Column (name = "surname")
     private String surname;
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public User(Long id, String name, String surname) {
+        Id = id;
+        this.name = name;
+        this.surname = surname;
+    }
 }
