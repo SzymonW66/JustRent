6181493d36dad919d358894e399ef613ed67ae9c
SzymonW66 <szymekwajs@gmail.com>
2023-04-14 00:24:30 +0200

    Delete findByUsername

--- src/main/java/com/JustRent/repositories/UserRepository.java
+++ src/main/java/com/JustRent/repositories/UserRepository.java
@@ -1,14 +1,10 @@
 package com.JustRent.repositories;
 
 import com.JustRent.models.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
     User findByEmail(String email);
 }
