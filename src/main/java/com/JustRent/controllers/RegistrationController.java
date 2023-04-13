8fa47c26439ce79b8864a8d20060438273078892
SzymonW66 <szymekwajs@gmail.com>
2023-04-14 00:56:32 +0200

    Adding @Controler for RegistrationController but dosent work (problem with Beans)

--- src/main/java/com/JustRent/controllers/RegistrationController.java
+++ src/main/java/com/JustRent/controllers/RegistrationController.java
@@ -3,24 +3,23 @@ package com.JustRent.controllers;
 import com.JustRent.dto.UserDto;
 import com.JustRent.exceptions.UserAlreadyExistException;
 import com.JustRent.models.User;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import com.JustRent.services.UserService;
-
+@Controller
 public class RegistrationController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/registration")
+    @GetMapping("/user/registration")
     public String showRegistrationForm(Model model) {
         UserDto userDto = new UserDto();
         model.addAttribute("user", userDto);
