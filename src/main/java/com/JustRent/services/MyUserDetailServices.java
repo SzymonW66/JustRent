c9e63e9aa429ed567e50fbdda9f5bb47c6242a0a
SzymonW66 <szymekwajs@gmail.com>
2023-04-20 23:05:37 +0200

    Adding and changes in HTML's and some minor delete

---
+++ src/main/java/com/JustRent/services/MyUserDetailServices.java
@@ -0,0 +1,46 @@
+package com.JustRent.services;
+import com.JustRent.models.User;
+import com.JustRent.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class MyUserDetailServices implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("No user found with username: " + email);
+        }
+        boolean enabled = true;
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), enabled, accountNonExpired,
+                credentialsNonExpired, accountNonLocked, getAuthorities(Collections.singletonList(user.getRole())));
+    }
+
+    private static List<GrantedAuthority> getAuthorities(List<String> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return authorities;
+    }
+}
